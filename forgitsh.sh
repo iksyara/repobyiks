76a301cfd6d7159ba9942c6b9989ebbd867f2f82
iksyara <71989483+iksyara@users.noreply.github.com>
2021-10-14 22:49:14 +0300

    Add files via upload

---
+++ forgitsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "this is script for github "
+num=$(($firstnumber + $secondnumber))
+#num=$firstnumber+$secondnumber
+#echo "$num (($firstnumber + $secondnumber))
+echo "num"
+echo "finish"
